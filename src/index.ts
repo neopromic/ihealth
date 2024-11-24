bc4524ddad39ba7de428baf8dfca7dc6e3f2b5d2
Timóteo  Bentes <bentestimoteo@gmail.com>
2024-11-24 11:37:43 -0400

    last changes

--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,5 @@ app.use(routes);
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
-  console.log("server running");
+  console.log(`Server is running on http://127.0.0.1:${PORT} 🚀`);
 });
